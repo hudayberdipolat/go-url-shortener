89bc981f9d8118beb076e8c117e2e7c8789e6824
hudayberdipolat <hudayberdipolat@gmail.com>
2024-01-27 09:02:13 +0500

    user auth response and request dto ready

---
+++ internal/domain/auth/dto/authResponse.go
@@ -0,0 +1,23 @@
+package dto
+
+import "github.com/hudayberdipolat/go-url-shortener/internal/models"
+
+type AuthResponse struct {
+	ID          int    `json:"id"`
+	Username    string `json:"username"`
+	FullName    string `json:"full_name"`
+	CreatedAt   string `json:"created_at"`
+	UpdatedAt   string `json:"updated_at"`
+	AccessToken string `json:"access_token"`
+}
+
+func NewAuthResponse(user models.User, accessToken string) AuthResponse {
+	return AuthResponse{
+		ID:          user.ID,
+		Username:    user.Username,
+		FullName:    user.FullName,
+		CreatedAt:   user.CreateAt.Format("01-02-2006"),
+		UpdatedAt:   user.UpdatedAt.Format("01-02-2006"),
+		AccessToken: accessToken,
+	}
+}
